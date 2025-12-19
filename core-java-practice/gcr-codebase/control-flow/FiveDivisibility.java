588a2dc4a0954ea7464baa24db95c6d4e7ac2ddb
jatin <jatinpratapsingh2002@gmail.com>
2025-12-19 23:07:09 +0530

    jatin add number divisible by 5

---
+++ core-java-practice/gcr-codebase/control-flow/FiveDivisibility.java
@@ -0,0 +1,9 @@
+ import java.util.Scanner;
+public class FiveDivisibility{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        boolean ischeck = (number % 5 == 0);
+        System.out.println("Is the number " + number + " divisible by 5? " + ischeck);
+    }
+}

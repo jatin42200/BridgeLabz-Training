a957952f06f7296bee14e4890ec925a714441315
jatin <jatinpratapsingh2002@gmail.com>
2025-12-20 01:05:30 +0530

    jatin add compute factorial of n number by using for loop

--- core-java-practice/gcr-codebase/control-flow/Compute_Factorial_Using_for_loop.java
+++ core-java-practice/gcr-codebase/control-flow/Compute_Factorial_Using_for_loop.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ComputeFactorial {
+public class Compute_Factorial_Using_for_loop {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -20,4 +20,5 @@ public class ComputeFactorial {
         }
     }
 }
+
 

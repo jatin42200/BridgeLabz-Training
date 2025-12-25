d7e8881f30ccacae4649f74e6e7362787686e686
jatin <jatinpratapsingh2002@gmail.com>
2025-12-25 22:42:47 +0530

    [Jatin Pratap Singh] Add . code of LEVEL --> 2 of programming elements

---
+++ core-java-practice/gcr-codebase/programming-elements/calc_SI.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+public class calc_SI {
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("input principal , rate and time");
+		double principal=sc.nextInt();
+		double rate=sc.nextInt();
+		double time=sc.nextInt();
+		double SI=(principal*rate*time)/100;
+		System.out.println("Simple Interest is ="+ SI);
+	}
+}

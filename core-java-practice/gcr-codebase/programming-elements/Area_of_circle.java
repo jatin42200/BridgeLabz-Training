23ce56cbb3902b77df6358ac923b1d27f4e70f51
jatin <jatinpratapsingh2002@gmail.com>
2025-12-25 22:33:14 +0530

    [Jatin Pratap Singh] Add . code of LEVEL --> 1 of programming elements

--- core-java-practice/gcr-codebase/programming-elements/Area_of_circle.java
+++ core-java-practice/gcr-codebase/programming-elements/Area_of_circle.java
@@ -1,6 +1,5 @@
-
-   import java.util.*;
-public class Area_of_circle {{
+import java.util.*;
+public class Area_of_circle {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("input the readius of the circle:");

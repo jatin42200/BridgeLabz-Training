d4fe51bdb48222b772b39bbd3271b83e57649867
jatin <jatinpratapsingh2002@gmail.com>
2025-12-25 22:04:16 +0530

    [Jatin Pratap Singh] Add . code of LEVEL --> 1 of programming elements

---
+++ core-java-practice/gcr-codebase/programming-elements/Sum_of_2_no.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+public class Sum_of_2_no {
+    public static void main(String[]args)
+    {
+         Scanner sc=new Scanner(System.in);
+         System.out.print("enter two number");
+         int a=sc.nextInt();
+         int b=sc.nextInt();
+         System.out.print("sum of two numbers in = "+(a+b));
+    }
+    
+}

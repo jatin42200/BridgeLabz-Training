0b71cb91474ad59de9adb2a5c125877854f4ce9f
jatin <jatinpratapsingh2002@gmail.com>
2025-12-29 13:12:55 +0530

    [Jatin Pratap Singh] Add . code of LEVEL --> 2 of Extra Built-In-Function

---
+++ core-java-practice/gcr-codebase/Method-Extra/GCD_and_LCM.java
@@ -0,0 +1,39 @@
+
+import java.util.Scanner;
+
+public class GCD_and_LCM {
+    // Function to calculate GCD 
+    static int calculateGCD(int a, int b) {
+
+        while (b != 0) {
+            int remainder = a % b;
+            a = b;
+            b = remainder;
+        }
+
+        return a;
+    }
+
+    // Function to calculate LCM
+    static int calculateLCM(int a, int b) {
+        return (a * b) / calculateGCD(a, b);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        int gcd = calculateGCD(num1, num2);
+        int lcm = calculateLCM(num1, num2);
+
+        System.out.println("GCD of " + num1 + " and " + num2 + " is: " + gcd);
+        System.out.println("LCM of " + num1 + " and " + num2 + " is: " + lcm);
+
+    }
+}

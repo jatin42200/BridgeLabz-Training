027bbbfb705604a95896b9263090fdfed942cc65
jatin <jatinpratapsingh2002@gmail.com>
2025-12-29 11:08:46 +0530

    [Jatin Pratap Singh] Add . code of LEVEL --> 1 of Extra Built-In-Function

--- core-java-practice/gcr-codebase/Method-Extra/TimeZone.java
+++ core-java-practice/gcr-codebase/Method-Extra/TimeZone.java
@@ -1,26 +1,14 @@
+import java.time.*;
 
-import java.time.ZonedDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
+public class TimeZone {
+    public static void main(String[] args) {
 
-public class TimeZone {    public static void main(String[] args) {
-        // Create a formatter to make the output readable
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss z");
-
-        // 1. GMT (Greenwich Mean Time)
         ZonedDateTime gmtTime = ZonedDateTime.now(ZoneId.of("GMT"));
-        
-        // 2. IST (Indian Standard Time)
         ZonedDateTime istTime = ZonedDateTime.now(ZoneId.of("Asia/Kolkata"));
-        
-        // 3. PST (Pacific Standard Time)
         ZonedDateTime pstTime = ZonedDateTime.now(ZoneId.of("America/Los_Angeles"));
 
-        // Display the results
-        System.out.println("Current Time in Different Time Zones:");
-        System.out.println("------------------------------------");
-        System.out.println("GMT: " + gmtTime.format(formatter));
-        System.out.println("IST: " + istTime.format(formatter));
-        System.out.println("PST: " + pstTime.format(formatter));
+        System.out.println("GMT Time : " + gmtTime);
+        System.out.println("IST Time : " + istTime);
+        System.out.println("PST Time : " + pstTime);
     }
 }

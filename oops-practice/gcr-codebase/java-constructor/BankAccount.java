448606d06068e26d10a7475bdb207c7bb596299b
jatin <jatinpratapsingh2002@gmail.com>
2025-12-31 23:19:17 +0530

    [Jatin Pratap Singh] Add . code of level 2 of java constructor of oops practice

---
+++ oops-practice/gcr-codebase/java-constructor/BankAccount.java
@@ -0,0 +1,22 @@
+class BankAccount {
+
+    public int accountNumber;
+    protected String accountHolder;
+    private double balance;
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
+class SavingsAccount extends BankAccount {
+
+    public void displayDetails() {
+        System.out.println(accountNumber);
+        System.out.println(accountHolder);
+    }
+}
+

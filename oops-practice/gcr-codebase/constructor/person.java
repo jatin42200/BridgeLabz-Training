4d93b1a42bb577fd63b453b253dcd1fbd76a1f5d
jatin <jatinpratapsingh2002@gmail.com>
2025-12-31 15:22:33 +0530

    [Jatin Pratap Singh] Add . code of level 1 of java constructor of oops practice

---
+++ oops-practice/gcr-codebase/constructor/person.java
@@ -0,0 +1,30 @@
+class CopyPerson {
+    String name;
+    int age;
+
+    CopyPerson(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Copy constructor
+    CopyPerson(CopyPerson p) {
+        this.name = p.name;
+        this.age = p.age;
+    }
+
+    public void display() {
+        System.out.println(name + " - " + age);
+    }
+}
+class Person{
+    public static void main(String[]args)
+    {
+          CopyPerson c1=new CopyPerson("goldy",22);
+          System.out.print("Original Person -> ");
+          c1.display();          
+          CopyPerson c2=new CopyPerson(c1);
+          System.out.print("copied person -> ");
+          c2.display();
+    }
+}

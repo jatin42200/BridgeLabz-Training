2fd83a79e414b6321430e7a4bb82dd5d1a4a8532
jatin <jatinpratapsingh2002@gmail.com>
2026-01-01 12:32:48 +0530

    [Jatin Pratap Singh] Add . code of level 1 of Kryboard

---
+++ oops-practice/gcr-codebase/keyboard/book.java
@@ -0,0 +1,38 @@
+public class checkInstance
+{
+	public static void main(String[] args) {
+		Book bs=new Book("the alchemist","jatin","970-4398765");
+		Book.displayLibraryName();
+		
+		if(bs instanceof Book)
+		{
+		    bs.displayDetail();
+		}
+		else{
+		    System.out.println("it is not instance of class book");
+		}
+		
+	}
+}
+class Book
+{
+    static String libraryName="vidhya Mandir";
+    private String title;
+    private String author;
+    private final String isbn;
+    public Book(String title,String author,String isbn)
+    {
+        this.title=title;
+        this.author=author;
+        this.isbn=isbn;
+    }
+    public static void displayLibraryName()
+    {
+         System.out.println("name of the library is : "+libraryName);
+    }
+    public void displayDetail()
+    {
+        System.out.println("isbn name is : "+isbn+"\nsuthor name is : "+author+"\nTitle of the book is : "+title);
+    }
+    
+}

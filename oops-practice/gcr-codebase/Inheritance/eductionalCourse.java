a41a950fa7cd6af0f4b1117f431353bc7a5bd3c3
jatin <jatinpratapsingh2002@gmail.com>
2026-01-03 15:47:01 +0530

    [Jatin Pratap Singh] Add . code of Level 1 of MultiLevel Inheritance

--- oops-practice/gcr-codebase/Inheritance/eductionalCourse.java
+++ oops-practice/gcr-codebase/Inheritance/eductionalCourse.java
@@ -1,3 +1,75 @@
+
+// Base class
+class Course {
+    String courseName;
+    int duration; // in hours
+
+    Course(String courseName, int duration) {
+        this.courseName = courseName;
+        this.duration = duration;
+    }
+
+    void displayDetails() {
+        System.out.println("Course Name: " + courseName);
+        System.out.println("Duration: " + duration + " hours");
+    }
+}
+
+// Subclass (Level 2)
+class OnlineCourse extends Course {
+    String platform;
+    boolean isRecorded;
+
+    OnlineCourse(String courseName, int duration, String platform, boolean isRecorded) {
+        super(courseName, duration);
+        this.platform = platform;
+        this.isRecorded = isRecorded;
+    }
+
+    @Override
+    void displayDetails() {
+        super.displayDetails();
+        System.out.println("Platform: " + platform);
+        System.out.println("Recorded: " + isRecorded);
+    }
+}
+
+// Subclass (Level 3)
+class PaidOnlineCourse extends OnlineCourse {
+    double fee;
+    double discount; // percentage
+
+    PaidOnlineCourse(String courseName, int duration, String platform,
+                     boolean isRecorded, double fee, double discount) {
+        super(courseName, duration, platform, isRecorded);
+        this.fee = fee;
+        this.discount = discount;
+    }
+
+    @Override
+    void displayDetails() {
+        super.displayDetails();
+        System.out.println("Fee: ₹" + fee);
+        System.out.println("Discount: " + discount + "%");
+        System.out.println("Final Price: ₹" + (fee - (fee * discount / 100)));
+    }
+}
+
+// Main class
 public class eductionalCourse {
-    
+    public static void main(String[] args) {
+
+        Course c1 = new Course("Java Basics", 40);
+        Course c2 = new OnlineCourse("DSA Online", 60, "Udemy", true);
+        Course c3 = new PaidOnlineCourse("Java Full Stack", 120,
+                "Coursera", true, 10000, 20);
+
+        c1.displayDetails();
+        System.out.println();
+
+        c2.displayDetails();
+        System.out.println();
+
+        c3.displayDetails();
+    }
 }

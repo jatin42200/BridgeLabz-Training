50d12f5e8013dad5c2ff19ec03b262f64bc0036c
Jatin <jatinpratapsingh2002@gmail.com>
2026-01-15 00:07:33 +0530

    [Jatin Pratap Singh] Add . leetcode 206

--- dsa-practice/leet-code-codebase/reverseLinkedList.java
+++ dsa-practice/leet-code-codebase/reverseLinkedList.java
@@ -1,5 +1,26 @@
-package dsa-practice.leet-code-codebase;
 
-public class reverseLinkedList {
-    
+  public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+ 
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        return reverse(head,null);       
+    }
+    public ListNode reverse(ListNode curr,ListNode prev)
+    {
+        if(curr==null)
+        {
+            return prev;
+        }
+        ListNode ans=reverse(curr.next,curr);
+        curr.next=prev;
+        return ans;
+        
+    }
+
 }
